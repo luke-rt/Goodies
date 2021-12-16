f87c20ae8bfd7150658bebd1d102c9cabfaefae5
ltgr <luke.rtong@gmail.com>
2021-12-16 14:32:07 -0500

    Business ops for customer vs business

--- FirstImplementationDraft/BusinessOperations.java
+++ FirstImplementationDraft/BusinessOperations.java
@@ -1,22 +1,59 @@
 package FirstImplementationDraft;
 
+import java.util.Scanner;
 import java.util.Map;
 
 public class BusinessOperations {
+    Scanner scanner;
     Restocker restocker;
 
     public BusinessOperations() {
+        this.scanner = new Scanner(System.in);
         this.restocker = new Restocker("data.txt");
     }
 
-    public void printBusinessMenu() {
+    public void prompt() {
+        System.out.println("""
+        Welcome to Goodies
+            1. Use as Customer
+            2. Use as Business Manager
+            3. Exit
+        """);
+
+        int option = this.scanner.nextInt();
+
+        switch(option) {
+            case 1:
+                handleCustomerRes();
+                break;
+            case 2:
+                handleBusinessRes();
+                break;
+            case 3:
+                System.exit(0);
+            default:
+                System.out.println("Invalid response");
+                break;
+        }
+    }
+
+    private void handleCustomerRes() {
+        printCustomerMenu();
+    }
+
+    private void handleBusinessRes() {
+        printBusinessMenu();
+    }
+
+
+    private void printBusinessMenu() {
         System.out.printf("%-12s%-10s%-8s%-1s\n", "Name", "Quantity", "Cost", "Sale Price");
         for(Map.Entry<String, Product> entry : this.restocker.stock.entrySet()) {
             System.out.printf("%-15s%-8d%-8d%f\n", entry.getKey(), entry.getValue().getQuantity(), entry.getValue().getCost(), entry.getValue().getPrice());
         }
     }
 
-    public void printCustomerMenu() {
+    private void printCustomerMenu() {
         System.out.printf("%-14s%s\n", "Name", "Sale Price");
         for(Map.Entry<String, Product> entry : this.restocker.stock.entrySet()) {
             System.out.printf("%-15s%f\n", entry.getKey(), entry.getValue().getPrice());
--- FirstImplementationDraft/GoodiesDriver.java
+++ FirstImplementationDraft/GoodiesDriver.java
@@ -3,7 +3,7 @@ package FirstImplementationDraft;
 public class GoodiesDriver {
     public static void main(String[] args) {
         BusinessOperations goodies = new BusinessOperations();
-        goodies.printBusinessMenu();
-        goodies.printCustomerMenu();
+        goodies.prompt();
+
     }
 }
